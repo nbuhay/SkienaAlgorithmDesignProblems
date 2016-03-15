fbe3aedb7026a912282affaa7d055e854b1d4b69
Nicholas Buhay <nbuhay@gmail.com>
2016-03-15 20:29:52 +0000

    exchanged tabs for spaces

--- Chapter_1/1_2_SelectingRightJobs/SelectingRightJobs.py
+++ Chapter_1/1_2_SelectingRightJobs/SelectingRightJobs.py
@@ -7,29 +7,32 @@ I =[];
 
 # compute the total time movie will take, append it to I
 def computeMovieLength(I):
-	for x in I:
-		x.append(x[1]-x[0]);
+  for x in I:
+    x.append(x[1]-x[0]);
 
 # populate the empty list I with n movie data points.  Movies have a start and end time where start always occurs before end.
 def genMovieTimeline(I, n):
-	I.clear();
-	for i in range(0, n):
-		start = int(Random.uniform(0,1) * 100);
-		end = Random.randint(start, 100);
-		I.append([start, end]);
-	computeMovieLength(I);
+  I.clear();
+  for i in range(0, n):
+    start = int(Random.uniform(0,1) * 100);
+    end = Random.randint(start, 100);
+    I.append([start, end]);
+  computeMovieLength(I);
 
 def buhay_sort(unsorted_list):
-        sorted_list = [];
-        sorted_list.append(unsorted_list.pop(0));
-        for x in unsorted_list:
-                popped = unsorted_list.pop(0);
-                i = 0;
-                while(i < len(sorted_list) and popped > sorted_list[i]):
-                        i += 1;
-                front = sorted_list[:i];
-                back = sorted_list[i:];
-                front.append(popped);
-                front.extend(back);
-                sorted_list = front;
-        return sorted_list;
+  sorted_list = [];
+  sorted_list.append(unsorted_list.pop(0));
+  for x in unsorted_list:
+    popped = unsorted_list.pop(0);
+    i = 0;
+    while(i < len(sorted_list) and popped > sorted_list[i]):
+      i += 1;
+    front = sorted_list[:i];
+    back = sorted_list[i:];
+    front.append(popped);
+    front.extend(back);
+    sorted_list = front;
+  return sorted_list;
+
+genMovieTimeline(I, 10);
+print(I);
