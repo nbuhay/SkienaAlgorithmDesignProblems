0841080ab5585db3ba5e1dbab8230bfcca96dc0d
Nicholas Buhay <nbuhay@gmail.com>
2016-03-16 15:51:36 +0000

    Sort complete - based on index of movie data object.  buhay_sort puts sorted list back into original list object

--- Chapter_1/1_2_SelectingRightJobs/SelectingRightJobs.py
+++ Chapter_1/1_2_SelectingRightJobs/SelectingRightJobs.py
@@ -10,7 +10,9 @@ def computeMovieLength(I):
   for x in I:
     x.append(x[1]-x[0]);
 
-# populate the empty list I with n movie data points.  Movies have a start and end time where start always occurs before end.
+# populate the empty list I with n movie data points.  
+# Movies have a start and end time where start always occurs before end
+# list element form:  [move_start, movie_end, delta_start_end]
 def genMovieTimeline(I, n):
   I.clear();
   for i in range(0, n):
@@ -19,19 +21,24 @@ def genMovieTimeline(I, n):
     I.append([start, end]);
   computeMovieLength(I);
 
-def buhay_sort(unsorted_list):
+# custom sort for movie data
+# returns ascending sort of list
+# usorted_list elements are of the form [move_start, movie_end, delta_start_end]
+# sort_element is the index of unsorted_list on which to sort
+def buhay_sort(unsorted_list, sort_element):
   sorted_list = [];
   sorted_list.append(unsorted_list.pop(0));
   for x in unsorted_list:
-    popped = unsorted_list.pop(0);
     i = 0;
-    while(i < len(sorted_list) and popped > sorted_list[i]):
+    while(i < len(sorted_list) and x[sort_element] > sorted_list[i][sort_element]):
       i += 1;
     front = sorted_list[:i];
     back = sorted_list[i:];
-    front.append(popped);
+    front.append(x);
     front.extend(back);
     sorted_list = front;
+  unsorted_list.clear();  # remove original unsorted list elements
+  unsorted_list.extend(sorted_list);  # copy sorted elements into original list object
   return sorted_list;
 
 genMovieTimeline(I, 10);
